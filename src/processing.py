74cf704f011ed42efbb83644c87c02e7c2479100
Ekaterina Pavlova <miss.kat.katerina2007@gmail.com>
2024-04-18 17:34:44 +0300

    created second function in processing.py

---
+++ src/processing.py
@@ -0,0 +1,27 @@
+from typing import Dict, List
+
+
+def filter_list_by_state(input_data: List[Dict[str, str | int]], state: str) -> List[Dict[str, str | int]]:
+    """
+    Принимает список словарей и значение для ключа и возвращает новый список, содержащий словари,
+    у которых ключ state содержит переданное в функцию значение
+    """
+    output_data = []
+    for dict_ in input_data:
+        if dict_["state"] == state:
+            output_data.append(dict_)
+    return output_data
+
+
+# def sort_data(input_data: List[Dict[str, str | int]]) -> List[Dict[str, str | int]]:
+#     """
+#     Принимает на вход список словарей и возвращает список, в котором исходные словари отсортированы по убыванию даты.
+#     """
+#     return sorted(input_data, key=lambda x: x.get("date"), reverse=True)
+
+
+def sort_data(input_data: List) -> List:
+    """
+    Принимает на вход список словарей и возвращает список, в котором исходные словари отсортированы по убыванию даты.
+    """
+    return sorted(input_data, key=lambda x: x.get("date"), reverse=True)
