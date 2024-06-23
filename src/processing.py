e124d95f6989c290f3e2c73da0010b6d873403b3
Ekaterina Pavlova <miss.kat.katerina2007@gmail.com>
2024-06-23 18:07:52 +0300

    small fix

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,20 @@
-from typing import Dict, List
+from typing import Any
 
 
-def filter_list_by_state(input_data: List[Dict[str, str | int]], state: str) -> List[Dict[str, str | int]]:
+def filter_list_by_state(input_data: Any, state: str) -> Any:
     """
     Принимает список словарей и значение для ключа и возвращает новый список, содержащий словари,
     у которых ключ state содержит переданное в функцию значение
     """
     output_data = []
     for dict_ in input_data:
-        if dict_["state"] == state:
+        if dict_.get("state", "") == state:
             output_data.append(dict_)
     return output_data
 
 
-def sort_data(input_data: List) -> List:
+def sort_data(input_data: Any, order: bool = False) -> Any:
     """
     Принимает на вход список словарей и возвращает список, в котором исходные словари отсортированы по убыванию даты.
     """
-    return sorted(input_data, key=lambda x: x.get("date"), reverse=True)
+    return sorted(input_data, key=lambda x: x.get("date"), reverse=order)
